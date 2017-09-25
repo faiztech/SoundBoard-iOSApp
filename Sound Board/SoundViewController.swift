e2d366a456df4badcbc5ec66206871d604a8fb76
Mohammed Faiz <mfaizudd@neiu.edu>
2017-09-25 12:14:10 -0500

    created screen2 UI and screen2ViewController

---
+++ Sound Board/SoundViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SoundViewController.swift
+//  Sound Board
+//
+//  Created by Mohammed Faizuddin on 9/25/17.
+//  Copyright © 2017 Faiz Tech. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+
+    @IBOutlet weak var recordButton: UIButton!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    
+    
+}
