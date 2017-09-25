9ed3e2b10e40c870ae3ea973d121762a97f09a06
Mohammed Faiz <mfaizudd@neiu.edu>
2017-09-25 11:39:02 -0500

    Initial Commit

---
+++ Sound Board/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sound Board
+//
+//  Created by Mohammed Faizuddin on 9/25/17.
+//  Copyright © 2017 Faiz Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
